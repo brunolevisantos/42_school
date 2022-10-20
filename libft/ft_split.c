eb0d8afe7876fb093821914c9a19636be1d82554
brunolevisantos <santos.b@me.com>
2022-10-20 18:43:44 +0100

    up to strtrim

---
+++ libft/ft_split.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-seic <bde-seic@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 18:03:08 by bde-seic          #+#    #+#             */
+/*   Updated: 2022/10/20 18:42:34 by bde-seic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// void	issplit(const char *s, char c)
+// {
+// 	int	*start;
+// 	int	*end;
+// 	int	i;
+		
+// 	i = 0;
+// 	while (s[i])
+// 	{
+// 		if (s[i] = c || s[i] = 0)
+// 		{
+// 			split = i;
+// 			return (split);
+// 			split = 0;
+// 		}
+// 		i++;
+// 	}
+// }
+
+char	**ft_split(char const *s, char c)
+{
+	char	**string;
+	int		num;
+
+	num = 0;
+	while ()
+	*string[num] = (char *)malloc(sizeof(char) * issplit(*s, c, issplit(*s, c, split)) + 1)
+}
+
+
+bruno levi santos
