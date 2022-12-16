7b326cc406bc756f8ac0139b60b7ee7a38a73798
Bruno Levi De Seica Santos <bde-seic@c1r7s1.42lisboa.com>
2022-12-16 14:27:48 +0000

    gnl hecho

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -1,32 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bde-seic <bde-seic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 10:33:35 by bde-seic          #+#    #+#             */
-/*   Updated: 2022/12/16 12:10:06 by bde-seic         ###   ########.fr       */
+/*   Updated: 2022/12/16 12:14:24 by bde-seic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*get_next_line(int fd)
 {
-	static char	buf[BUFFER_SIZE + 1];
+	static char	buf[1024][BUFFER_SIZE + 1];
 	char		*line;
 
 	line = 0;
-	if (buf[0])
+	if (buf[fd][0])
 	{
-		line = ft_strjoin(line, buf);
-		move_buf(buf);
+		line = ft_strjoin(line, buf[fd]);
+		move_buf(buf[fd]);
 	}
-	while (!ft_strchr(line, '\n') && read(fd, buf, BUFFER_SIZE) > 0)
+	while (!ft_strchr(line, '\n') && read(fd, buf[fd], BUFFER_SIZE) > 0)
 	{
-		line = ft_strjoin(line, buf);
-		move_buf(buf);
+		line = ft_strjoin(line, buf[fd]);
+		move_buf(buf[fd]);
 	}
 	return (line);
 }
--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -1,32 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bde-seic <bde-seic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/15 10:33:35 by bde-seic          #+#    #+#             */
-/*   Updated: 2022/12/16 12:10:06 by bde-seic         ###   ########.fr       */
+/*   Created: 2022/12/09 10:13:20 by bde-seic          #+#    #+#             */
+/*   Updated: 2022/12/16 12:14:31 by bde-seic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
-char	*get_next_line(int fd)
-{
-	static char	buf[BUFFER_SIZE + 1];
-	char		*line;
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
-	line = 0;
-	if (buf[0])
-	{
-		line = ft_strjoin(line, buf);
-		move_buf(buf);
-	}
-	while (!ft_strchr(line, '\n') && read(fd, buf, BUFFER_SIZE) > 0)
-	{
-		line = ft_strjoin(line, buf);
-		move_buf(buf);
-	}
-	return (line);
-}
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+int		ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+void	move_buf(char *buf);
+
+#endif
