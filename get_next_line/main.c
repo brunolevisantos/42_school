331c6ec415ffb7a0e694771d3305c35160dcb1f3
Bruno Levi De Seica Santos <bde-seic@c1r7s1.42lisboa.com>
2022-12-16 11:21:30 +0000

    clean up

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: bde-seic <bde-seic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 10:24:10 by bde-seic          #+#    #+#             */
-/*   Updated: 2022/12/15 16:06:36 by bde-seic         ###   ########.fr       */
+/*   Updated: 2022/12/16 11:18:37 by bde-seic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ int	main(void)
 	int		fd;
 	char	*str;
 
-	//fd = open("file_to_read.txt", O_RDONLY);
-	fd = open("read_error.txt", O_RDONLY);
+	fd = open("file_to_read.txt", O_RDONLY);
 	str = get_next_line(fd);
 	while (str)
 	{
