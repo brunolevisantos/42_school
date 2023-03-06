e01bde1e45b627d3c5243d3d8c8d3c9b324a5d4c
brunolevisantos <113634009+brunolevisantos@users.noreply.github.com>
2023-03-06 00:11:42 +0000

    pipex starting

--- pipex/SRCS/pipex.c
+++ pipex/SRCS/pipex.c
@@ -6,7 +6,7 @@
 /*   By: bde-seic <bde-seic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 16:01:46 by bde-seic          #+#    #+#             */
-/*   Updated: 2023/03/02 16:12:41 by bde-seic         ###   ########.fr       */
+/*   Updated: 2023/03/06 00:10:29 by bde-seic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,33 @@
 
 int	main(int argc, char **argv)
 {
-	int	fd;
-
-	if (argc > 5)
+	// ver uso de char *envp[]
+	int	fd[2];
+	int	pid;
+	int	i;
+	char	cmd[] = "/usr/bin/wc"; //corrigir para nome do progarma certo
+	char	*argVec[] = {"wc", "-l", argv[1], NULL};
+	char	*envVec[] = {NULL};
+	
+	i = -1;
+	if (argc > 2) //corrigir p maior que 5
 	{
-		fd = open(argv[1], O_RDONLY);
-		
+		if (pipe(fd) == -1)
+			perror ("pipe");
+		pid = fork();
+		if (pid == -1)
+			perror ("fork");
+		else if (pid == 0)
+		{
+			fd[0] = open(argv[1], O_RDONLY);
+			while (argv[++i])
+			fd[1] = open(argv[i - 1], O_WRONLY | O_CREAT, 0777); //ver como apagar o que la esta
+			dup2(fd[1], 1);
+			printf("Start of execve call %s\n", cmd);
+			if (execve(cmd, argVec, envVec) == -1)
+				perror("could not execute\n");
+		}
+		else if (pid != 0) //esta sempre a fazer o parent process 
+			printf("Something went wrong\n"); 
 	}
 }
