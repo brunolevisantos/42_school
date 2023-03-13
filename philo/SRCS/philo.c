06c5934ef994e7446879e07b80b1a22d3e0ca44e
brunolevisantos <113634009+brunolevisantos@users.noreply.github.com>
2023-03-13 18:40:19 +0000

    learning threads

---
+++ philo/SRCS/philo.c
@@ -0,0 +1,29 @@
+#include "../include/philo.h"
+
+void	*roll_dice()
+{	
+	int value;
+	int *result;
+
+	value = (rand() % 6) + 1;
+	result = malloc (sizeof(int));
+	*result = value;
+	// printf("%d\n", value);
+	return (result);
+}
+
+int	main(int ac, char **av)
+{
+	(void) ac;
+	(void) av;
+	pthread_t t1;
+	int *res;
+
+	srand(time(0));
+	if (pthread_create(&t1, 0, &roll_dice, 0) != 0)
+		printf("error on thrad creation\n");
+	if (pthread_join(t1, &res) != 0)
+		printf("error on thread waiting\n");
+	preintf("result: %d\n", *res);
+	return (0);
+}
---
+++ philo/include/philo.h
@@ -0,0 +1,12 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <pthread.h>
+# include <time.h>
+
+#endif
