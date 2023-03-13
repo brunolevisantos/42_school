784603eba8c1e5476327c35109121fa76aed4f9a
brunolevisantos <113634009+brunolevisantos@users.noreply.github.com>
2023-03-13 23:47:20 +0000

    passing arguments and returning values

--- philo/SRCS/philo.c
+++ philo/SRCS/philo.c
@@ -1,30 +1,52 @@
 #include "../include/philo.h"
 
-void	*roll_dice()
-{	
-	int value;
-	int *result;
+int primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
 
-	value = (rand() % 6) + 1;
-	result = malloc (sizeof(int));
-	*result = value;
-	// printf("%d\n", value);
-	return ((void*) result);
+void	*routine(void *arg)
+{	
+	int	index;
+	int	j;
+	int	sum;
+
+	index = *(int*)arg;
+	j = 0;
+	sum = 0;
+	while (j < 5)
+		sum += primes[index + j++];
+	printf ("local sum: %d\n", sum);
+	*(int *)arg = sum;
+	return (arg);
 }
 
 int	main(int ac, char **av)
 {
 	(void) ac;
 	(void) av;
-	pthread_t t1;
-	int *res;
+	pthread_t th[2];
+	int	i;
+	int	*a;
+	int	*res;
+	int	globalSum;
 
-	srand(time(0));
-	if (pthread_create(&t1, 0, &roll_dice, 0) != 0)
-		printf("error on thrad creation\n");
-	if (pthread_join(t1, (void **) &res) != 0)
-		printf("error on thread waiting\n");
-	printf("result: %d\n", *res);
-	free (res);
+	globalSum = 0;
+	// i = 0;
+	// while (i++ < 2)
+	for (i = 0; i < 2; i++)
+	{
+		a = malloc(sizeof(int));
+		*a = i * 5;
+		if (pthread_create(&th[i], 0, &routine, a) != 0)
+			perror ("Failed to create thread");
+	}
+	// i = 0;
+	// while (i++ < 2)
+	for (i = 0; i < 2; i++)
+	{
+		if (pthread_join(th[i], (void **) &res) != 0)
+			perror ("Failed to join thread");
+		globalSum += *res;
+		free(res);
+	}
+	printf("global sum: %d\n", globalSum);
 	return (0);
 }
